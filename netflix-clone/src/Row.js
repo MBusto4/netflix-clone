c95e3c1ad5f6563aa3c51df8b921f1f7c11112fa
MBusto <mikebusto4@gmail.com>
2021-10-15 15:22:57 -0400

    styling all the movie images

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import "./Row.css"
 import axios from './axios'
 import requests from './requests'
+import './Row.css'
 
 
 const base_url = 'https://image.tmdb.org/t/p/original/'
@@ -36,8 +37,10 @@ function Row({ title, fetchUrl }) {
             {/* Container */}
             <div className="row__posters">
                 {movies.map((movie) => (
-                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
-
+                    <img
+                        key={movie.id}
+                        className='row__poster'
+                        src={`${base_url}${movie.poster_path}`} alt={movie.name} />
                 ))}
             </div>
 
